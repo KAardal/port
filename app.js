8d840187040f9c3397ec073f14380ebdc92d576a
Kyle Aardal <aardal.kyle@gmail.com>
2017-05-11 08:45:41 -0700

    added toHtml() method

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 'use strict';
 
-function main() {
-
-}
-
 function Project(projectData) {
   this.name = projectData.name;
   this.image = projectData.image;
-  this.discription = projectData.discription;
+  this.description = projectData.description;
   this.repoURL = projectData.repoURL;
 }
 
 Project.prototype.toHtml = function () {
   var $newProject = $('div.template').clone();
-  
+  $newProject.removeClass('template');
+  $newProject.find('.project-name').html(this.name);
+  $newProject.find('.project-img').attr('src', this.image);
+  $newProject.find('.project-description').html(this.description);
+  $newProject.find('project-repo').attr('src', this.repoURL);
+  return $newProject;
 };
-
-main();
