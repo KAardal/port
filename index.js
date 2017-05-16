3961ab179157835a1fb032f521a7e4f8af80d5b7
Kyle Aardal <aardal.kyle@gmail.com>
2017-05-16 16:17:24 -0700

    express.static() in index.js now points to the correct directory

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 'use strict';
 // Load Express
 const express = require('express');
-
-// Instantiate Express so that we can use its functionality
 const app = express();
-
-// Designate a port to serve our app on
 const PORT = process.env.PORT || 5000;
 
-// Define which directory we will serve files from
-app.use(express.static('./data'));
-
-// // Here is one way to set up a route... bats...
-// app.get('/bats', function(request, response) {
-//   console.log('Damn bats');
-//   response.sendFile('public/bat-country.html', {root: '.'});
-// })
+app.use(express.static('.'));
 
 app.get('*', function(request, response) {
   console.log('404: file not found');
