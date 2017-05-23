ebdb43dc4e8dbf2a16149be5e66e686f5c7ed198
Kyle Aardal <aardal.kyle@gmail.com>
2017-05-23 08:52:17 -0700

    added iife

--- scripts/app.js
+++ scripts/app.js
@@ -2,90 +2,95 @@
 
 var app = app || {};
 
-function main() {
-  loadData();
-  handleBurgerMenu();
-  handleWindowResize();
-  handleMainNav();
-  projectCounter();
-}
-
-let projects = [];
-
-function Project(projectData) {
-  this.name = projectData.name;
-  this.image = projectData.image;
-  this.description = projectData.description;
-  this.repoURL = projectData.repoURL;
-}
-
-Project.prototype.toHtml = function () {
-  let template = Handlebars.compile($('#project-template').text());
-  return template(this);
-};
-
-function renderProjects(data) {
-  data.forEach(function(projectObject) {
-    projects.push(new Project(projectObject));
-  })
-
-  projects.forEach(function(project){
-    $('#projects').append(project.toHtml());
-  });
-}
-
-function loadData () {
-  if(localStorage.projectData) {
-    renderProjects(JSON.parse(localStorage.projectData));
-  } else {
-    $.getJSON('/data/projectData.json')
-      .then(function(data) {
-        localStorage.setItem('projectData', JSON.stringify(data));
-        renderProjects(data);
-      }, function(err) {
-        console.error(err);
-      });
-  }
-}
-
-function handleBurgerMenu() {
-  $('#burger-open').on('click', function(e) {
-    e.preventDefault();
-    $('.main-nav').slideDown();
-  });
-
-  $('#burger-close').on('click', function() {
-    $('.main-nav').slideUp();
-  });
-}
-
-function handleWindowResize() {
-  if($(window).width() < 640) {
-    $('.main-nav').slideUp(0);
+(function(module){
+  function main() {
+    app.loadData();
+    app.handleBurgerMenu();
+    app.handleWindowResize();
+    app.handleMainNav();
+    app.projectCounter();
   }
 
-  $(window).on('resize', function() {
-    if($(window).width() < 640) {
-      $('#burger-close').click();
+  let projects = [];
+
+  app.Project= function(projectData) {
+    this.name = projectData.name;
+    this.image = projectData.image;
+    this.description = projectData.description;
+    this.repoURL = projectData.repoURL;
+  }
+
+  app.Project.prototype.toHtml = function () {
+    let template = Handlebars.compile($('#project-template').text());
+    return template(this);
+  };
+
+  app.renderProjects = function(data) {
+    data.forEach(function(projectObject) {
+      projects.push(new app.Project(projectObject));
+    })
+
+    projects.forEach(function(project){
+      $('#projects').append(project.toHtml());
+    });
+  }
+
+  app.loadData = function() {
+    if(localStorage.projectData) {
+      app.renderProjects(JSON.parse(localStorage.projectData));
     } else {
-      $('#burger-open').click();
+      $.getJSON('/data/projectData.json')
+        .then(function(data) {
+          localStorage.setItem('projectData', JSON.stringify(data));
+          app.renderProjects(data);
+        }, function(err) {
+          console.error(err);
+        });
     }
-  })
-}
+  }
 
-function handleMainNav() {
-  $('.main-nav li').on('click', function(e) {
-    e.preventDefault();
-    $('.tab').hide();
-    $('#' + $(this).data('nav')).fadeIn();
-  });
-}
+  app.handleBurgerMenu = function() {
+    $('#burger-open').on('click', function(e) {
+      e.preventDefault();
+      $('.main-nav').slideDown();
+    });
 
-function projectCounter() {
-  $('#footer ').text(projects
-    .map(project => project.description.split(' '))
-    .reduce((acc, cur) => acc += cur.length, 0)
-  );
-}
+    $('#burger-close').on('click', function() {
+      $('.main-nav').slideUp();
+    });
+  }
 
-main();
+  app.handleWindowResize = function() {
+    if($(window).width() < 640) {
+      $('.main-nav').slideUp(0);
+    }
+
+    $(window).on('resize', function() {
+      if($(window).width() < 640) {
+        $('#burger-close').click();
+      } else {
+        $('#burger-open').click();
+      }
+    })
+  }
+
+  app.handleMainNav = function() {
+    $('.main-nav li').on('click', function(e) {
+      e.preventDefault();
+      $('.tab').hide();
+      $('#' + $(this).data('nav')).fadeIn();
+    });
+  }
+
+  app.projectCounter = function() {
+    $('#footer ').text(projects
+      .filter(project => project.body)
+      .map(body => body.split(' '))
+      .reduce((acc, cur) => acc + cur, 0)
+    );
+  }
+
+  main();
+
+  app = module;
+})(app);
