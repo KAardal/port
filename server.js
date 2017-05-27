3f468f9bf87ed0a63c4d58af80b6c6c6997f7325
Kyle Aardal <aardal.kyle@gmail.com>
2017-05-27 11:56:53 -0700

    fixed routing in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static('.'));
+app.use(express.static('./public'));
 
 app.get('*', (request, response) => response.sendFile('index.html', {root: './public'}));
 
